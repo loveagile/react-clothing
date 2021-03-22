f37a7c3734a092094fcc6432efc394e3dcb155ee
loveagile <happyplandry@gmail.com>
2021-03-22 11:51:25 -0400

    Set up Shop page and collection-preview

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/ShopPage';
+
 
 import './App.css';
-
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE </h1>
-    </div>
-);
 
 function App() {
     return (
         <div>
             <Switch>
                 <Route exact path='/' component={HomePage}/>
-                <Route path='/hats' component={HatsPage}/>
+                <Route path='/shop' component={ShopPage}/>
             </Switch>
         </div>
     );
