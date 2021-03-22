c45ee968774b3c6f91cded649c8ca17fc8941c49
loveagile <happyplandry@gmail.com>
2021-03-22 09:56:22 -0400

    Build homepage and directory components

---
+++ src/components/menu-item/MenuItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import "./menu-item.scss"
+
+const MenuItem = ({ title, imageUrl, size }) => {
+    return (
+        <div
+            style={{backgroundImage: `url(${imageUrl}`}}
+            className={`${size} menu-item`}
+        >
+            <div className='content'>
+                <div className='title'>{title}</div>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
+        </div>
+    );
+};
+
+export default MenuItem;
+
---
+++ src/pages/homepage/homepage.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Directory from '../../components/directory/Directory';
+
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <h1>Welcome to my Homepage</h1>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
