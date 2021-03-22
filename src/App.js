f856a3107cf49ae0fde8772c1b612e79aabb0589
loveagile <happyplandry@gmail.com>
2021-03-22 11:03:30 -0400

    Add react router dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
 
-class App extends React.Component {
-  render() {
+const HatsPage = () => (
+    <div>
+        <h1>HATS PAGE </h1>
+    </div>
+);
+
+function App() {
     return (
-      <div className='App'>
-        <HomePage />
-      </div>
+        <div>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route path='/hats' component={HatsPage}/>
+            </Switch>
+        </div>
     );
-  }
 }
+
 
 export default App;
