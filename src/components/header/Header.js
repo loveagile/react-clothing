b123289c0fc278f381517a64b9ce0a31b2561631
loveagile <happyplandry@gmail.com>
2021-03-23 19:18:39 -0400

    Add cartIcon and CartDropdown

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom"
 import { connect } from "react-redux"
 
 import { auth } from "../../firebase/firebase.utils"
+import CartIcon from "../cart-icon/CartIcon"
+import CartDropdown from "../cart-dropdown/CartDropdown"
 
 import { ReactComponent as Logo } from "../../assets/crown.svg"
-
 import "./header.scss"
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -29,12 +30,15 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 )
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header)
---
+++ src/redux/cart/cart.action.js
@@ -0,0 +1,6 @@
+import { CartActionTypes } from "./cart.types"
+
+export const toggleCartHidden = (user) => ({
+  type: CartActionTypes.TOGGLE_CART_HIDDEN,
+  payload: user
+})
