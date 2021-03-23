ba8a4b01636ce730005c50d340ceefe9374f3cd0
loveagile <happyplandry@gmail.com>
2021-03-23 16:57:08 -0400

    Add redux, and implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,62 +1,58 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react"
+import { Switch, Route } from "react-router-dom"
+import { connect } from "react-redux"
 
-import HomePage from './pages/homepage/homepage.component';
-import Header from "./components/header/Header";
-import ShopPage from './pages/shop/ShopPage';
-import SignInSignUp from './pages/sign-in-and-sign-up/SignInSignUp';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import "./App.css"
 
-import './App.css';
+import HomePage from "./pages/homepage/homepage.component"
+import ShopPage from "./pages/shop/ShopPage"
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/SignInSignUp"
+import Header from "./components/header/Header"
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
+import { setCurrentUser } from "./redux/user/user.action"
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
-  unsubscribeFromAuth = null;
+  unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+    const { setCurrentUser } = this.props
 
-        userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
-          });
-        });
-      } else {
-        this.setState({ currentUser: userAuth });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+
+        userRef.onSnapshot((snapShot) => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          })
+        })
       }
-    });
+
+      setCurrentUser(userAuth)
+    })
   }
 
-
   componentWillUnmount() {
-    this.unsubscribeFromAuth();
+    this.unsubscribeFromAuth()
   }
 
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
-          <Route exact path='/' component={ HomePage }/>
-          <Route path='/shop' component={ ShopPage }/>
-          <Route path='/signin' component={ SignInSignUp }/>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
         </Switch>
       </div>
     )
   }
 }
 
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user))
+})
 
-export default App;
+export default connect(null, mapDispatchToProps)(App)
--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -1,72 +1,71 @@
-import React from 'react';
+import React from "react"
 
-import FormInput from '../form-input/FormInput';
-import CustomButton from '../custom-button/CustomButton';
+import FormInput from "../form-input/FormInput"
+import CustomButton from "../custom-button/CustomButton"
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils"
 
-import './sign-in.scss';
+import "./sign-in.scss"
 
 class SignIn extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-
-    this.state = { email: '', password: '' };
+    this.state = { email: "", password: "" }
   }
 
-  handleSubmit = async event => {
-    event.preventDefault();
+  handleSubmit = async (event) => {
+    event.preventDefault()
 
-    const { email, password } = this.state;
+    const { email, password } = this.state
 
     try {
-      await auth.signInWithEmailAndPassword(email, password);
-      this.setState({ email: '', password: '' });
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({ email: "", password: "" })
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
-  handleChange = event => {
-    const { value, name } = event.target;
+  handleChange = (event) => {
+    const { value, name } = event.target
 
-    this.setState({ [name]: value });
-  };
+    this.setState({ [name]: value })
+  }
 
   render() {
     return (
-      <div className='sign-in'>
+      <div className="sign-in">
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
           <FormInput
-            name='email'
-            type='email'
+            name="email"
+            type="email"
             handleChange={this.handleChange}
             value={this.state.email}
-            label='email'
+            label="email"
             required
           />
           <FormInput
-            name='password'
-            type='password'
+            name="password"
+            type="password"
             value={this.state.password}
             handleChange={this.handleChange}
-            label='password'
+            label="password"
             required
           />
-          <div className='buttons'>
-            <CustomButton type='submit'> Sign in </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit"> Sign in </CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
               Sign in with Google
             </CustomButton>
           </div>
         </form>
       </div>
-    );
+    )
   }
 }
 
-export default SignIn;
+export default SignIn
