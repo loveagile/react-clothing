85f103f551efb47538b3ca7b2b53e26696faf788
loveagile <happyplandry@gmail.com>
2021-03-24 13:50:10 -0400

    Finish collection page

--- src/pages/shop/ShopPage.js
+++ src/pages/shop/ShopPage.js
@@ -1,29 +1,15 @@
-import React from 'react';
-import CollectionPreview from '../../components/collection-preview/CollectionPreview';
+import React from "react";
+import { Route } from "react-router-dom";
 
-import SHOP_DATA from './ShopData';
+import CollectionsOverview from '../../components/collections-overview/CollectionsOverview';
+import CollectionPage from '../../pages/collection/CollectionPage';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-
-  render() {
-    const { collections } = this.state
-    return (
-      <div className="shop-page">
-        {
-          collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-          ))
-        }
-      </div>
-    );
-  }
-}
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
 export default ShopPage;
