a97a8c909685a5915cf938af7dc9de4527ae159f
loveagile <happyplandry@gmail.com>
2021-03-25 08:40:12 -0400

    Set up husky pre-commit hooks to lint code

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
   apiKey: "AIzaSyBDUD5qE5NmkjqO87J8EfqZFsFOhFpBHt8",
@@ -31,18 +31,18 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData
       });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
 
   return userRef;
-}
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firestore;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,19 @@
-import { createStore, applyMiddleware } from "redux"
-import { persistStore } from 'redux-persist';
+import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 
-import logger from "redux-logger"
+import logger from "redux-logger";
 
-import rootReducer from "./root-reducer"
+import rootReducer from "./root-reducer";
 
-const middlewares = [logger]
+const middlewares = [logger];
 
-if (process.env.NODE_ENV === 'development') {
-  middlewares.push(logger)
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default { store, persistStore };
